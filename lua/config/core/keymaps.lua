fd6eddea3bb6f74b8ddce9aab389190c27e84b2c
huibosa_wsl <huibosa@gmail.com>
2023-09-06 18:55:58 +0800

    init: first commit

---
+++ lua/config/core/keymaps.lua
@@ -0,0 +1,89 @@
+-- Disable Space key (map it to Nop)
+vim.api.nvim_set_keymap("n", "<Space>", "<Nop>", { noremap = true, silent = true })
+
+-- Set mapleader to Space
+vim.g.mapleader = " "
+
+vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
+vim.api.nvim_set_keymap("x", "Y", '"+y', { noremap = true })
+
+vim.api.nvim_set_keymap("n", "Q", ":q!<CR>", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "<", "<<", { noremap = true })
+vim.api.nvim_set_keymap("n", ">", ">>", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "c*", "*Ncgn", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "H", "^", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "L", "$", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "H", "^", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "L", "$", { noremap = true, silent = true })
+
+-- Keeping j and k silent in normal mode
+vim.api.nvim_set_keymap("n", "j", "j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "k", "k", { noremap = true, silent = true })
+
+-- Resize mappings
+vim.api.nvim_set_keymap("n", "<Up>", ":res +5<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Down>", ":res -5<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Left>", ":vertical resize-5<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Right>", ":vertical resize+5<CR>", { noremap = true })
+
+-- Emulate <C-a> as vscode <Home> key
+vim.api.nvim_set_keymap("i", "<C-a>", "", {
+	callback = function()
+		local current_line = vim.fn.getline(".")
+		local current_col = vim.fn.col(".")
+		local non_blank_column = string.find(current_line, "%S") or 1
+
+		-- Check if the cursor is at the start of the line or non-blank character
+		if current_col == 1 or vim.fn.match(current_line, "\\S", current_col - 1) == -1 then
+			-- Cursor is at the start of the line or before a non-blank character
+			-- Move to the first non-blank character
+			vim.fn.cursor(current_line, non_blank_column)
+		else
+			-- Cursor is at a non-blank character
+			-- Move to the start of the line
+			vim.fn.cursor(current_line, 1)
+		end
+	end,
+	noremap = true,
+	silent = true,
+})
+
+-- Map <C-k> to kill line like emacs
+vim.api.nvim_set_keymap("i", "<C-k>", "", {
+	callback = function()
+		local current_line = vim.fn.getline(".")
+		local cursor_col = vim.fn.col(".")
+		local line_length = #current_line
+
+		-- Check if there are characters to delete after the cursor
+		if cursor_col <= line_length then
+			-- Delete the characters
+			vim.fn.setline(vim.fn.line("."), current_line:sub(1, cursor_col - 1))
+
+			-- Move the cursor to the end of the modified text
+			vim.fn.cursor(vim.fn.line("."), cursor_col)
+		end
+	end,
+})
+
+vim.api.nvim_set_keymap("i", "<C-e>", "<End>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-b>", "<Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<C-f>", "<Right>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<C-d>", "<Del>", { noremap = true })
+
+-- Command-line mappings
+vim.api.nvim_set_keymap("c", "<C-a>", "<Home>", { noremap = true })
+vim.api.nvim_set_keymap("c", "<C-e>", "<End>", { noremap = true })
+vim.api.nvim_set_keymap("c", "<C-b>", "<Left>", { noremap = true })
+vim.api.nvim_set_keymap("c", "<C-f>", "<Right>", { noremap = true })
+vim.api.nvim_set_keymap("c", "<C-d>", "<Del>", { noremap = true })
+
+-- Buffer navigation mappings
+vim.api.nvim_set_keymap("n", "]b", ":bnext<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "[b", ":bprev<CR>", { noremap = true })
+
+-- Delete buffer without closing split
+vim.api.nvim_set_keymap("n", "<leader>d", ":bp<Bar>bd #<CR>", { noremap = true, silent = true })
---
+++ lua/config/core/options.lua
@@ -0,0 +1,101 @@
+-- Check if in a git repo
+local git_status = vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null")
+if git_status then
+	vim.opt.signcolumn = "yes"
+else
+	vim.opt.signcolumn = "no"
+end
+
+-- Change fillchars for folding, vertical split, end of buffer, and message separator
+vim.opt.fillchars:append({ fold = " ", vert = "│", eob = " ", msgsep = "‾" })
+
+-- Ignore certain files and folders when globbing
+vim.opt.wildignore:append({ "*.o", "*.obj", "*.dylib", "*.bin", "*.dll", "*.exe" })
+vim.opt.wildignore:append({ "*/.git/*", "*/.svn/*", "*/__pycache__/*", "*/build/**" })
+vim.opt.wildignore:append({ "*.jpg", "*.png", "*.jpeg", "*.bmp", "*.gif", "*.tiff", "*.svg", "*.ico" })
+vim.opt.wildignore:append({ "*.pyc", "*.pkl", "*.DS_Store" })
+vim.opt.wildignorecase = true
+
+vim.opt.swapfile = false
+
+-- Set up backup directory
+vim.g.backupdir = vim.fn.expand(vim.fn.stdpath("data") .. "/backup//")
+--vim.opt.backupdir = vim.g.backupdir
+
+-- Skip backup for patterns in option wildignore
+--vim.opt.backupskip = vim.opt.wildignore
+
+-- Editor behavior
+vim.opt.filetype = "plugin"
+vim.opt.number = true
+vim.opt.hidden = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.inccommand = "nosplit"
+vim.opt.scrolloff = 5
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wildmenu = true
+
+-- General tab settings
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+
+vim.opt.foldmethod = "syntax"
+vim.opt.foldenable = false
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.encoding = "utf-8"
+vim.opt.showmode = false
+vim.opt.updatetime = 500
+vim.opt.cmdheight = 0
+
+-- Set matching pairs of characters and highlight matching brackets
+vim.opt.matchpairs:append({ "「:」", "『:』", "【:】", "“:”", "‘:’", "《:》" })
+
+vim.opt.mouse = ""
+
+-- Break line at predefined characters
+vim.opt.linebreak = true
+vim.opt.showbreak = "↪"
+
+vim.opt.autowrite = true
+vim.opt.undofile = true
+vim.opt.shortmess:append({ c = true, S = true, I = true })
+
+vim.opt.shiftround = true
+vim.opt.virtualedit = "block"
+vim.opt.formatoptions:append({ m = true, M = true })
+
+-- Tilde (~) is an operator
+vim.opt.tildeop = true
+vim.opt.synmaxcol = 250
+
+-- External program to use for grep command
+if vim.fn.executable("rg") then
+	vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+	vim.opt.grepformat = "%f:%l:%c:%m"
+end
+
+vim.opt.termguicolors = true
+
+-- Set up cursor color and shape in various modes
+vim.opt.guicursor = "n-v-c:block-Cursor/lCursor,i-ci-ve:ver25-Cursor2/lCursor2,r-cr:hor20,o:hor20"
+
+-- Remove certain characters from file name pattern matching
+vim.opt.isfname:remove({ "-", "=" })
+vim.opt.isfname:remove({ "," })
+
+-- Diff options
+vim.opt.diffopt = {}
+vim.opt.diffopt:append("vertical") -- Show diff in vertical position
+vim.opt.diffopt:append("filler") -- Show filler for deleted lines
+vim.opt.diffopt:append("closeoff") -- Turn off diff when one file window is closed
+vim.opt.diffopt:append("context:3") -- Context for diff
+vim.opt.diffopt:append("internal,indent-heuristic,algorithm:histogram")
+
+vim.opt.wrap = false
+vim.opt.ruler = false
