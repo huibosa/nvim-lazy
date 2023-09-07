1d869931f87a0e9fefc7d882a33cb21ba842a0d3
huibosa_wsl <huibosa@gmail.com>
2023-09-07 17:01:01 +0800

    improve: set window_borders as global variable

--- init.lua
+++ init.lua
@@ -1,21 +1,2 @@
 require("config.core")
 require("config.lazy")
-
-local border = {
-  { "┏", "FloatBorder" },
-  { "━", "FloatBorder" },
-  { "┓", "FloatBorder" },
-  { "┃", "FloatBorder" },
-  { "┛", "FloatBorder" },
-  { "━", "FloatBorder" },
-  { "┗", "FloatBorder" },
-  { "┃", "FloatBorder" },
-}
-
--- To instead override globally
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-  opts = opts or {}
-  opts.border = opts.border or border
-  return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
--- lua/config/plugins/nvim-cmp.lua
+++ lua/config/plugins/nvim-cmp.lua
@@ -1,74 +1,63 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		"L3MON4D3/LuaSnip", -- snippet engine
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",         -- source for text in buffer
+    "hrsh7th/cmp-path",           -- source for file system paths
+    "L3MON4D3/LuaSnip",           -- snippet engine
+    "saadparwaiz1/cmp_luasnip",   -- for autocompletion
+    "rafamadriz/friendly-snippets", -- useful snippets
+    "onsails/lspkind.nvim",       -- vs-code like pictograms
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
 
-		local border = {
-			{ "┏", "FloatBorder" },
-			{ "━", "FloatBorder" },
-			{ "┓", "FloatBorder" },
-			{ "┃", "FloatBorder" },
-			{ "┛", "FloatBorder" },
-			{ "━", "FloatBorder" },
-			{ "┗", "FloatBorder" },
-			{ "┃", "FloatBorder" },
-		}
+    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    require("luasnip.loaders.from_vscode").lazy_load()
 
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
-
-		cmp.setup({
-			window = {
-				completion = { border = "none", scrollbar = true },
-				documentation = { border = border, scrollbar = true },
-			},
-			completion = {
-				completeopt = "menu,menuone,preview",
-			},
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-			-- sources for autocompletion
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
-			}),
-			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "text",
-					maxwidth = 50,
-					ellipsis_char = "...",
-					menu = {
-						nvim_lsp = "[LSP]",
-						luasnip = "[LuaSnip]",
-						path = "[Path]",
-						buffer = "[Buffer]",
-					},
-				}),
-			},
-		})
-	end,
+    cmp.setup({
+      window = {
+        completion = { border = "none", scrollbar = true },
+        documentation = { border = vim.g.window_borders, scrollbar = true },
+      },
+      completion = {
+        completeopt = "menu,menuone,preview",
+      },
+      snippet = { -- configure how nvim-cmp interacts with snippet engine
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<C-e>"] = cmp.mapping.abort(),    -- close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }),
+      -- sources for autocompletion
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- snippets
+        { name = "buffer" }, -- text within current buffer
+        { name = "path" }, -- file system paths
+      }),
+      -- configure lspkind for vs-code like pictograms in completion menu
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "text",
+          maxwidth = 50,
+          ellipsis_char = "...",
+          menu = {
+            nvim_lsp = "[LSP]",
+            luasnip = "[LuaSnip]",
+            path = "[Path]",
+            buffer = "[Buffer]",
+          },
+        }),
+      },
+    })
+  end,
 }
