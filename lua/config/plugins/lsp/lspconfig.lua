c0494e2e889b87564adcb9a307914ea6cb46aa57
huibosa_macmini <huibosa@email.com>
2023-09-07 20:08:42 +0800

    feat: add cmp-nvim-lua

--- lua/config/plugins/lsp/lspconfig.lua
+++ lua/config/plugins/lsp/lspconfig.lua
@@ -1,155 +1,155 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    -- import lspconfig plugin
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		local on_attach = function(client, bufnr)
-			-- Mappings.
-			local map = function(mode, l, r, opts)
-				opts = opts or {}
-				opts.silent = true
-				opts.buffer = bufnr
-				vim.keymap.set(mode, l, r, opts)
-			end
+    local on_attach = function(client, bufnr)
+      -- Mappings.
+      local map = function(mode, l, r, opts)
+        opts = opts or {}
+        opts.silent = true
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
 
-			-- set keybinds
-			map("n", "gR", "<cmd>Telescope lsp_references<CR>", { desc = "Show LSP references" })
-			map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-			map("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Show LSP definitions" }) -- show lsp definitions
-			map("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" })
-			map("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Show LSP type definitions" })
-			map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions" })
-			map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Smart rename" })
-			map("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Show buffer diagnostics" })
-			map("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
-			map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-			map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-			map("n", "K", vim.lsp.buf.hover, { desc = "Show documentation for what is under cursor" })
-			map("n", "<leader>rs", "<cmd>LspRestart<CR>", { desc = "Restart LSP" })
+      -- set keybinds
+      map("n", "gR", "<cmd>Telescope lsp_references<CR>", { desc = "Show LSP references" })
+      map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+      map("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Show LSP definitions" }) -- show lsp definitions
+      map("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" })
+      map("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Show LSP type definitions" })
+      map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions" })
+      map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Smart rename" })
+      map("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Show buffer diagnostics" })
+      map("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+      map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+      map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+      map("n", "K", vim.lsp.buf.hover, { desc = "Show documentation for what is under cursor" })
+      map("n", "<leader>rs", "<cmd>LspRestart<CR>", { desc = "Restart LSP" })
 
-			-- Set some key bindings conditional on server capabilities
-			if client.server_capabilities.documentFormattingProvider then
-				map("n", "==", vim.lsp.buf.format, { desc = "format code" })
-			end
+      -- Set some key bindings conditional on server capabilities
+      if client.server_capabilities.documentFormattingProvider then
+        map("n", "==", vim.lsp.buf.format, { desc = "format code" })
+      end
 
-			vim.api.nvim_create_autocmd("CursorHold", {
-				buffer = bufnr,
-				callback = function()
-					local float_opts = {
-						focusable = false,
-						close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-						border = "rounded",
-						source = "always", -- show source in diagnostic popup window
-						prefix = " ",
-					}
+      vim.api.nvim_create_autocmd("CursorHold", {
+        buffer = bufnr,
+        callback = function()
+          local float_opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = "rounded",
+            source = "always", -- show source in diagnostic popup window
+            prefix = " ",
+          }
 
-					if not vim.b.diagnostics_pos then
-						vim.b.diagnostics_pos = { nil, nil }
-					end
+          if not vim.b.diagnostics_pos then
+            vim.b.diagnostics_pos = { nil, nil }
+          end
 
-					local cursor_pos = vim.api.nvim_win_get_cursor(0)
-					if
-						(cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
-						and #vim.diagnostic.get() > 0
-					then
-						vim.diagnostic.open_float(nil, float_opts)
-					end
+          local cursor_pos = vim.api.nvim_win_get_cursor(0)
+          if
+              (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+              and #vim.diagnostic.get() > 0
+          then
+            vim.diagnostic.open_float(nil, float_opts)
+          end
 
-					vim.b.diagnostics_pos = cursor_pos
-				end,
-			})
+          vim.b.diagnostics_pos = cursor_pos
+        end,
+      })
 
-			-- The blow command will highlight the current variable and its usages in the buffer.
-			if client.server_capabilities.documentHighlightProvider then
-				vim.cmd([[
+      -- The blow command will highlight the current variable and its usages in the buffer.
+      if client.server_capabilities.documentHighlightProvider then
+        vim.cmd([[
       hi! link LspReferenceRead Visual
       hi! link LspReferenceText Visual
       hi! link LspReferenceWrite Visual
     ]])
 
-				local gid = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-				vim.api.nvim_create_autocmd("CursorHold", {
-					group = gid,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.document_highlight()
-					end,
-				})
+        local gid = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+        vim.api.nvim_create_autocmd("CursorHold", {
+          group = gid,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.document_highlight()
+          end,
+        })
 
-				vim.api.nvim_create_autocmd("CursorMoved", {
-					group = gid,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.clear_references()
-					end,
-				})
-			end
-		end
+        vim.api.nvim_create_autocmd("CursorMoved", {
+          group = gid,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.clear_references()
+          end,
+        })
+      end
+    end
 
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+    -- used to enable autocompletion (assign to every lsp server config)
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+    -- Change the Diagnostic symbols in the sign column (gutter)
+    local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
 
-		-- configure python server
-		lspconfig["pyright"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- configure python server
+    lspconfig["pyright"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		lspconfig["clangd"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			filetypes = { "c", "cpp", "cc", "h", "hpp" },
-			flags = {
-				debounce_text_changes = 500,
-			},
-		})
+    lspconfig["clangd"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      filetypes = { "c", "cpp", "cc", "h", "hpp" },
+      flags = {
+        debounce_text_changes = 500,
+      },
+    })
 
-		lspconfig["bashls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
+    lspconfig["bashls"].setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
 
-		lspconfig["gopls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
+    lspconfig["gopls"].setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
 
-		-- configure lua server (with special settings)
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = { -- custom settings for lua
-				Lua = {
-					runtime = {
-						-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-						version = "LuaJIT",
-					},
-					-- make the language server recognize "vim" global
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						-- make language server aware of runtime files
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
-						},
-					},
-				},
-			},
-		})
-	end,
+    -- configure lua server (with special settings)
+    lspconfig["lua_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = { -- custom settings for lua
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = "LuaJIT",
+          },
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            -- make language server aware of runtime files
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+          },
+        },
+      },
+    })
+  end,
 }
--- lua/config/plugins/nvim-cmp.lua
+++ lua/config/plugins/nvim-cmp.lua
@@ -2,12 +2,13 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    "hrsh7th/cmp-buffer",         -- source for text in buffer
-    "hrsh7th/cmp-path",           -- source for file system paths
-    "L3MON4D3/LuaSnip",           -- snippet engine
-    "saadparwaiz1/cmp_luasnip",   -- for autocompletion
+    "hrsh7th/cmp-buffer",           -- source for text in buffer
+    "hrsh7th/cmp-path",             -- source for file system paths
+    "L3MON4D3/LuaSnip",             -- snippet engine
+    "hrsh7th/cmp-nvim-lua",         -- source for nvim lua completetion
+    "saadparwaiz1/cmp_luasnip",     -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim",       -- vs-code like pictograms
+    "onsails/lspkind.nvim",         -- vs-code like pictograms
   },
   config = function()
     local cmp = require("cmp")
@@ -22,28 +23,33 @@ return {
         completion = { border = "none", scrollbar = true },
         documentation = { border = vim.g.window_borders, scrollbar = true },
       },
+
       completion = {
         completeopt = "menu,menuone,preview",
       },
+
       snippet = { -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
+
       mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(),    -- close completion window
+        ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
-      -- sources for autocompletion
+
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = "nvim_lua" },
         { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
+        { name = "buffer" },  -- text within current buffer
+        { name = "path" },    -- file system paths
       }),
+
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
