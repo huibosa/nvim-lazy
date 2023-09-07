37513bc54318a22d5f731f5137ea65667d2bf112
huibosa_macmini <huibosa@email.com>
2023-09-08 07:34:34 +0800

    improve: dressing.vim window blend to 0

--- lua/config/plugins/dressing.lua
+++ lua/config/plugins/dressing.lua
@@ -1,4 +1,13 @@
 return {
-	"stevearc/dressing.nvim",
-	event = "VeryLazy",
+  "stevearc/dressing.nvim",
+  event = "VeryLazy",
+  config = function()
+    local dressing = require("dressing")
+    dressing.setup({
+      input = {
+        win_options = { winblend = 0 },
+        border = "double"
+      }
+    })
+  end
 }
