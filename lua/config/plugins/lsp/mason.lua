91b5f48ca7fbafaf414a29c4c027be16a8135a5a
huibosa_wsl <huibosa@gmail.com>
2023-09-07 16:00:13 +0800

    remove: null-ls

--- lua/config/plugins/lsp/mason.lua
+++ lua/config/plugins/lsp/mason.lua
@@ -2,12 +2,10 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
-		"jayp0521/mason-null-ls.nvim",
 	},
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
-		local mason_null_ls = require("mason-null-ls")
 
 		-- enable mason and configure icons
 		mason.setup({
@@ -32,15 +30,6 @@ return {
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
-		})
-
-		mason_null_ls.setup({
-			-- list of formatters & linters for mason to install
-			ensure_installed = {
-				"stylua", -- lua formatter
-			},
-			-- auto-install configured servers (with lspconfig)
-			automatic_installation = true,
 		})
 	end,
 }
