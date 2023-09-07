72f1c74536d624fc00ff63b146939d4e53a939f5
huibosa_wsl <huibosa@gmail.com>
2023-09-07 16:50:43 +0800

    format: use spaces instead of tabs

--- init.lua
+++ init.lua
@@ -2,20 +2,20 @@ require("config.core")
 require("config.lazy")
 
 local border = {
-	{ "┏", "FloatBorder" },
-	{ "━", "FloatBorder" },
-	{ "┓", "FloatBorder" },
-	{ "┃", "FloatBorder" },
-	{ "┛", "FloatBorder" },
-	{ "━", "FloatBorder" },
-	{ "┗", "FloatBorder" },
-	{ "┃", "FloatBorder" },
+  { "┏", "FloatBorder" },
+  { "━", "FloatBorder" },
+  { "┓", "FloatBorder" },
+  { "┃", "FloatBorder" },
+  { "┛", "FloatBorder" },
+  { "━", "FloatBorder" },
+  { "┗", "FloatBorder" },
+  { "┃", "FloatBorder" },
 }
 
 -- To instead override globally
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-	opts = opts or {}
-	opts.border = opts.border or border
-	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
