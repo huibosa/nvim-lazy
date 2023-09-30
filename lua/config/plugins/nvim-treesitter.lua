b18e052905f17700c2f2085e082c3c8d94caa4a4
huibosa_macmini <huibosa@email.com>
2023-09-30 17:30:51 +0800

    feat(treesitter): incremental selection

--- lua/config/plugins/nvim-treesitter.lua
+++ lua/config/plugins/nvim-treesitter.lua
@@ -33,6 +33,15 @@ return {
                 -- Instead of true it can also be a list of languages
                 addtional_vim_regex_highlighting = false,
             },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<CR>",
+                    scope_incremental = "<CR>",
+                    node_incremental = "v",
+                    node_decremental = "V",
+                },
+            },
         })
     end,
 }
