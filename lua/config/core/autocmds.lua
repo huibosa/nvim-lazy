197193068d6a82a80b29c8510e428462c1c99be4
huibosa_macmini <huibosa@email.com>
2023-10-01 17:01:28 +0800

    feat(autocmds): restore last cursor position

--- lua/config/core/autocmds.lua
+++ lua/config/core/autocmds.lua
@@ -1,3 +1,29 @@
+-- From vim defaults.vim
+-- ---
+-- When editing a file, always jump to the last known cursor position.
+-- Don't do it when the position is invalid, when inside an event handler
+-- (happens when dropping a file on gvim) and for a commit message (it's
+-- likely a different one than last time).
+vim.api.nvim_create_autocmd("BufRead", {
+    callback = function(opts)
+        vim.api.nvim_create_autocmd("BufWinEnter", {
+            once = true,
+            buffer = opts.buf,
+            callback = function()
+                local ft = vim.bo[opts.buf].filetype
+                local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+                if
+                    not (ft:match("commit") and ft:match("rebase"))
+                    and last_known_line > 1
+                    and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+                then
+                    vim.api.nvim_feedkeys([[g`"]], "nx", false)
+                end
+            end,
+        })
+    end,
+})
+
 -- highlight yanked region, see `:h lua-highlight`
 local yank_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
