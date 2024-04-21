ec872d55d514d457d8377ddfe18f941441cb606d
huibosa_wsl <huibosa@gmail.com>
2024-04-21 12:07:26 +0800

    feat(treesitter-context)

--- lua/config/plugins/vim-matchup.lua
+++ lua/config/plugins/vim-matchup.lua
@@ -7,7 +7,7 @@ return {
         vim.g.matchup_text_obj_enabled = 0
         vim.g.matchup_motion_enabled = 0
 
-        vim.g.matchup_matchparen_offscreen = { method = "popup" }
+        -- vim.g.matchup_matchparen_offscreen = { method = "popup" }
         vim.g.matchup_surround_enabled = 1
         vim.g.matchup_delim_noskips = 2 -- don't recognize anything in comments
     end,
