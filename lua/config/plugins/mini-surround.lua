2ae3ac6bcd3a79452d1607d7d416220bc5eb9128
huibosa_wsl <huibosa@gmail.com>
2024-05-17 10:33:12 +0800

    feat(surround): use mini.surround

---
+++ lua/config/plugins/mini-surround.lua
@@ -0,0 +1,16 @@
+return {
+    "echasnovski/mini.surround",
+    event = { "BufReadPre", "BufNewFile" },
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    opts = {
+        mappings = {
+            add = "gsa", -- Add surrounding in Normal and Visual modes
+            delete = "gsd", -- Delete surrounding
+            find = "gsf", -- Find surrounding (to the right)
+            find_left = "gsF", -- Find surrounding (to the left)
+            highlight = "gsh", -- Highlight surrounding
+            replace = "gsr", -- Replace surrounding
+            update_n_lines = "gsn", -- Update `n_lines`
+        },
+    },
+}
