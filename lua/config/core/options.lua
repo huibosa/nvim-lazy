2a6cb06898e9130d50d8a1fce89bb5e69792e3f9
huibosa_wsl <huibosa@gmail.com>
2024-05-19 20:19:40 +0800

    style: opt = vim.opt

--- lua/config/core/options.lua
+++ lua/config/core/options.lua
@@ -1,91 +1,93 @@
-vim.opt.mouse = "" -- disable the mouse
-vim.opt.termguicolors = true -- enable 24bit colors
+local opt = vim.opt
 
-vim.opt.updatetime = 300 -- decrease updatetime
-vim.opt.fileformat = "unix" -- <NL> for EOL
-vim.opt.fileencoding = "utf-8" -- encoding for reading and writing files
-vim.opt.encoding = "utf-8" -- encoding for specifying text
-vim.opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
+opt.mouse = "" -- disable the mouse
+opt.termguicolors = true -- enable 24bit colors
 
-vim.opt.cmdheight = 1 -- cmdline height
-vim.opt.scrolloff = 10 -- min number of lines to keep between cursor and screen edge
-vim.opt.sidescrolloff = 5 -- min number of cols to keep between cursor and screen edge
-vim.opt.number = true
+opt.updatetime = 300 -- decrease updatetime
+opt.fileformat = "unix" -- <NL> for EOL
+opt.fileencoding = "utf-8" -- encoding for reading and writing files
+opt.encoding = "utf-8" -- encoding for specifying text
+opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
 
-vim.opt.signcolumn = "yes"
-vim.opt.numberwidth = 3
-vim.opt.stc = "%=%l%s"
+opt.cmdheight = 1 -- cmdline height
+opt.scrolloff = 10 -- min number of lines to keep between cursor and screen edge
+opt.sidescrolloff = 5 -- min number of cols to keep between cursor and screen edge
+opt.number = true
+
+opt.signcolumn = "yes"
+opt.numberwidth = 3
+opt.stc = "%=%l%s"
 
 -- General tab settings
-vim.opt.tabstop = 2 -- Tab indentation levels every two columns
-vim.opt.softtabstop = 2 -- Tab indentation when mixing tabs & spaces
-vim.opt.shiftwidth = 2 -- Indent/outdent by two columns
-vim.opt.shiftround = true -- Always indent/outdent to nearest tabstop
-vim.opt.expandtab = true -- Convert all tabs that are typed into spaces
+opt.tabstop = 2 -- Tab indentation levels every two columns
+opt.softtabstop = 2 -- Tab indentation when mixing tabs & spaces
+opt.shiftwidth = 2 -- Indent/outdent by two columns
+opt.shiftround = true -- Always indent/outdent to nearest tabstop
+opt.expandtab = true -- Convert all tabs that are typed into spaces
 
-vim.opt.swapfile = false
+opt.swapfile = false
 
 -- Set up backup directory
 vim.g.backupdir = vim.fn.expand(vim.fn.stdpath("data") .. "/backup//")
---vim.opt.backupdir = vim.g.backupdir
+--opt.backupdir = vim.g.backupdir
 
 -- Skip backup for patterns in option wildignore
---vim.opt.backupskip = vim.opt.wildignore
+--opt.backupskip = opt.wildignore
 
 -- Editor behavior
-vim.opt.filetype = "plugin"
-vim.opt.hidden = true
+opt.filetype = "plugin"
+opt.hidden = true
 
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
+opt.hlsearch = true
+opt.incsearch = true
 
-vim.opt.inccommand = "nosplit"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.wildmenu = true
+opt.inccommand = "nosplit"
+opt.ignorecase = true
+opt.smartcase = true
+opt.wildmenu = true
 
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+opt.autoindent = true
+opt.smartindent = true
 
-vim.opt.foldmethod = "syntax"
-vim.opt.foldenable = false
+opt.foldmethod = "syntax"
+opt.foldenable = false
 
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.showmode = true
+opt.splitright = true
+opt.splitbelow = true
+opt.showmode = true
 
 -- Break line at predefined characters
-vim.opt.linebreak = true
-vim.opt.showbreak = "↪"
+opt.linebreak = true
+opt.showbreak = "↪"
 
-vim.opt.autowrite = true
-vim.opt.undofile = true
-vim.opt.shortmess = "acosOCIF"
+opt.autowrite = true
+opt.undofile = true
+opt.shortmess = "acosOCIF"
 
-vim.opt.virtualedit = "block"
-vim.opt.formatoptions:append({ m = true, M = true })
+opt.virtualedit = "block"
+opt.formatoptions:append({ m = true, M = true })
 
 -- External program to use for grep command
 if vim.fn.executable("rg") then
-    vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
-    vim.opt.grepformat = "%f:%l:%c:%m"
+    opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+    opt.grepformat = "%f:%l:%c:%m"
 end
 
 -- Set up cursor color and shape in various modes
--- vim.opt.guicursor = "n-v-c:block-Cursor/lCursor,i-ci-ve:ver25-Cursor2/lCursor2,r-cr:hor20,o:hor20"
-vim.opt.guicursor = "i:block"
+-- opt.guicursor = "n-v-c:block-Cursor/lCursor,i-ci-ve:ver25-Cursor2/lCursor2,r-cr:hor20,o:hor20"
+opt.guicursor = "i:block"
 
 -- Remove certain characters from file name pattern matching
-vim.opt.isfname:remove({ "," })
+opt.isfname:remove({ "," })
 
-vim.opt.wrap = false
-vim.opt.ruler = false
+opt.wrap = false
+opt.ruler = false
 
-vim.opt.laststatus = 0
+opt.laststatus = 0
 
 -- Change fillchars for folding, vertical split, end of buffer, and message separator
-vim.opt.fillchars:append({ fold = " ", vert = "┃", eob = " ", msgsep = "‾" })
-vim.opt.fillchars:append({
+opt.fillchars:append({ fold = " ", vert = "┃", eob = " ", msgsep = "‾" })
+opt.fillchars:append({
     horiz = "━",
     horizup = "┻",
     horizdown = "┳",
@@ -96,19 +98,19 @@ vim.opt.fillchars:append({
 })
 
 -- Ignore certain files and folders when globbing
-vim.opt.wildignore:append({ "*.o", "*.obj", "*.dylib", "*.bin", "*.dll", "*.exe" })
-vim.opt.wildignore:append({ "*/.git/*", "*/.svn/*", "*/__pycache__/*", "*/build/**" })
-vim.opt.wildignore:append({ "*.jpg", "*.png", "*.jpeg", "*.bmp", "*.gif", "*.tiff", "*.svg", "*.ico" })
-vim.opt.wildignore:append({ "*.pyc", "*.pkl", "*.DS_Store" })
-vim.opt.wildignorecase = true
+opt.wildignore:append({ "*.o", "*.obj", "*.dylib", "*.bin", "*.dll", "*.exe" })
+opt.wildignore:append({ "*/.git/*", "*/.svn/*", "*/__pycache__/*", "*/build/**" })
+opt.wildignore:append({ "*.jpg", "*.png", "*.jpeg", "*.bmp", "*.gif", "*.tiff", "*.svg", "*.ico" })
+opt.wildignore:append({ "*.pyc", "*.pkl", "*.DS_Store" })
+opt.wildignorecase = true
 
 -- Diff options
-vim.opt.diffopt = {}
-vim.opt.diffopt:append("vertical") -- Show diff in vertical position
-vim.opt.diffopt:append("filler") -- Show filler for deleted lines
-vim.opt.diffopt:append("closeoff") -- Turn off diff when one file window is closed
-vim.opt.diffopt:append("context:3") -- Context for diff
-vim.opt.diffopt:append("internal,indent-heuristic,algorithm:histogram")
+opt.diffopt = {}
+opt.diffopt:append("vertical") -- Show diff in vertical position
+opt.diffopt:append("filler") -- Show filler for deleted lines
+opt.diffopt:append("closeoff") -- Turn off diff when one file window is closed
+opt.diffopt:append("context:3") -- Context for diff
+opt.diffopt:append("internal,indent-heuristic,algorithm:histogram")
 
 ------------------------------------------------------------------------
 --                         builtin variables                          --
