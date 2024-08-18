cc8f6765bbb215725d4ceb81f89bc2e014115bd3
huibosa_macmini <huibosa@email.com>
2024-08-19 06:47:10 +0800

    feat(quicker)

--- lua/config/plugins/nvim-bqf.lua
+++ lua/config/plugins/nvim-bqf.lua
@@ -1,10 +1,15 @@
 return {
     "kevinhwang91/nvim-bqf",
     event = "FileType qf",
-    opts = {
-        preview = {
-            border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
-            show_title = false,
-        },
-    },
+    config = function()
+        vim.api.nvim_set_hl(0, "BqfPreviewFloat", { link = "FloatBorder" })
+
+        require("bqf").setup({
+            preview = {
+                border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+                show_title = false,
+                winblend = 0,
+            },
+        })
+    end,
 }
