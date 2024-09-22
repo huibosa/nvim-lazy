50343cd6045e1c0d67979167ff4fa5d1b356fb1a
huibosa_wsl <huibosa@gmail.com>
2024-09-22 15:22:42 +0800

    feat(gitsigns): keybinding, diffthis

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -2,7 +2,6 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
-        attach_to_untracked = true,
         worktrees = {
             {
                 toplevel = vim.env.HOME,
@@ -28,31 +27,36 @@ return {
             end
 
             -- Actions for stage
-            map("n", "<LEADER>ghs", gs.stage_hunk, { desc = "Stage Hunk" })
-            map("n", "<LEADER>ghr", gs.reset_hunk, { desc = "Reset Hunk" })
+            map("n", "<LEADER>hs", gs.stage_hunk, { desc = "Stage Hunk" })
+            map("n", "<LEADER>hr", gs.reset_hunk, { desc = "Reset Hunk" })
             map(
                 "v",
-                "<LEADER>ghs",
+                "<LEADER>hs",
                 function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
                 { desc = "Stage Hunk" }
             )
             map(
                 "v",
-                "<LEADER>ghr",
+                "<LEADER>hr",
                 function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
                 { desc = "Reset Hunk" }
             )
-            map("n", "<LEADER>ghS", gs.stage_buffer, { desc = "Stage Buffer" })
-            map("n", "<LEADER>ghu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
-            map("n", "<LEADER>ghR", gs.reset_buffer, { desc = "Reset Buffer" })
+            map("n", "<LEADER>hS", gs.stage_buffer, { desc = "Stage Buffer" })
+            map("n", "<LEADER>hu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+            map("n", "<LEADER>hR", gs.reset_buffer, { desc = "Reset Buffer" })
 
-            map("n", "<LEADER>ghp", gs.preview_hunk_inline, { desc = "Preview hunk Inline" })
-            map("n", "<LEADER>ghb", function() gs.blame_line({ full = true }) end, { desc = "Blame Line" })
-            map("n", "<LEADER>ghd", gs.diffthis, { desc = "Diff This" })
-            map("n", "<LEADER>ghD", function() gs.diffthis("~") end, { desc = "Diff This ~" })
+            map("n", "<LEADER>hP", gs.preview_hunk_inline, { desc = "Preview hunk Inline" })
+            map("n", "<LEADER>hp", gs.preview_hunk, { desc = "Preview hunk" })
+            map("n", "<LEADER>hb", function() gs.blame_line({ full = true }) end, { desc = "Blame Line" })
+            map("n", "<LEADER>hd", gs.diffthis, { desc = "Diff current change" })
+            map("n", "<LEADER>hD", ":<C-u>Gitsigns diffthis ", { desc = "Diff with given commit" })
 
-            -- map("n", "<LEADER>ght", gs.toggle_current_line_blame, { desc = "Blame line toggle" })
-            -- map("n", "<LEADER>ght", gs.toggle_deleted, { desc = "Toggle hunk" })
+            map("n", "<LEADER>htb", gs.toggle_current_line_blame, { desc = "Blame" })
+            map("n", "<LEADER>htd", gs.toggle_deleted, { desc = "Deleted hunk" })
+            map("n", "<LEADER>hth", gs.toggle_linehl, { desc = "Line highlight" })
+            map("n", "<LEADER>htn", gs.toggle_numhl, { desc = "Linenr highlight" })
+            map("n", "<LEADER>hts", gs.toggle_signs, { desc = "Signs" })
+            map("n", "<LEADER>htw", gs.toggle_word_diff, { desc = "Word diff" })
 
             map({ "o", "x" }, "ih", ":<C-u>Gitsigns select_hunk<CR>", { desc = "Hunk" })
         end,
