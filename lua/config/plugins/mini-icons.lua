5f59a73ffe9df53f9619d35c6a58b291c565bfd7
huibosa_wsl <huibosa@gmail.com>
2025-08-12 11:09:39 +0800

    feat(icon): use mini.icons

---
+++ lua/config/plugins/mini-icons.lua
@@ -0,0 +1,8 @@
+return {
+    "echasnovski/mini.icons",
+    lazy = false,
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("mini.icons").setup()
+    end
+}
