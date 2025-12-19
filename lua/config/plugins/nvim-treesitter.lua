2c44c8bc7135b1cc3e501cd80f1aaa8651d2b343
huibosa_wsl <huibosa@gmail.com>
2025-12-19 10:51:47 +0800

    chore(treesitter): add some parsers

--- lua/config/plugins/nvim-treesitter.lua
+++ lua/config/plugins/nvim-treesitter.lua
@@ -4,10 +4,45 @@ return {
     build = ":TSUpdate",
     branch = "main",
     config = function()
-        require('nvim-treesitter').install({ 'rust', 'go', 'c', 'cpp', 'python' })
+        local ts = require('nvim-treesitter')
+
+        ts.install({
+            'rust', 'go', 'c', 'cpp', 'python', 'lua',
+            'bash',
+            'comment',
+            'css',
+            'diff',
+            'fish',
+            'git_config',
+            'git_rebase',
+            'gitcommit',
+            'gitignore',
+            'html',
+            'javascript',
+            'json',
+            'latex',
+            'luadoc',
+            'make',
+            'markdown',
+            'markdown_inline',
+            'norg',
+            'query',
+            'regex',
+            'scss',
+            'svelte',
+            'toml',
+            'tsx',
+            'typescript',
+            'typst',
+            'vim',
+            'vimdoc',
+            'vue',
+            'xml',
+        })
 
         vim.opt.foldmethod = "expr"
         vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
         -- Enable highlight
         vim.api.nvim_create_autocmd('FileType', {
