01d981fedb4da6e50cccc08cce2d27a03c3546db
huibosa_wsl <huibosa@gmail.com>
2026-02-09 09:26:32 +0800

    refactor(basedpyright): update config path and add autoFormatStrings
    - Move useLibraryCodeForTypes to correct analysis path
    - Add autoFormatStrings for automatic f-string conversion

--- after/lsp/basedpyright.lua
+++ after/lsp/basedpyright.lua
@@ -37,6 +37,7 @@ return {
                     pytestParameters = true,
                     genericTypes = true,
                 },
+                autoFormatStrings = true,
             },
         },
     },
