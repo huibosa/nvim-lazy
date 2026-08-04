bb82c7bdfd3183f2479ff0e97e0a54b427b48bc7
huibosa <huibosa@gmail.com>
2026-08-04 17:53:14 +0800

    feat(autopairs): disable backtick autopairing rules

--- lua/config/plugins/nvim-autopairs.lua
+++ lua/config/plugins/nvim-autopairs.lua
@@ -12,5 +12,14 @@ return {
             },
             map_c_h = true,
         })
+
+        -- Disable all backtick autocompletion:
+        --   "```"      closing fence when typing the third backtick (markdown etc.)
+        --   "```.*$"   fence auto-close on Enter (regex rule)
+        --   "`"        single-backtick autopairing
+        -- run after setup so the default basic/ts rules exist to remove
+        autopairs.remove_rule("```")
+        autopairs.remove_rule("```.*$")
+        autopairs.remove_rule("`")
     end,
 }
